cf355dbd51961fa47e3bf8bb38921d11cf274960
ram5353 <ramkiran5353@gmail.com>
2020-03-08 21:24:59 +0530

    Initial commit

---
+++ src/SingleLinkedList.java
@@ -0,0 +1,101 @@
+import java.util.LinkedList;
+
+public class SingleLinkedList {
+    public SingleNode head;
+    public SingleNode tail;
+    public int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public SingleNode CreateSingleLinkedList(int nodevalue) {
+        head = new SingleNode();
+        SingleNode node = new SingleNode();
+        node.setValue(nodevalue);
+        node.setNext(null);
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    public void insertInLinkedList(int nodeValue, int location) {
+        SingleNode node = new SingleNode();
+        node.setValue(nodeValue);
+        if (!existsLinkedList()) { // Linked List does not exists
+            System.out.println("The linked list does not exist!!");
+            return;
+        } else if (location == 0) {// insert at first position
+            node.setNext(head);
+            head = node;
+        } else if (location >= size) {// insert at last position
+            node.setNext(null);
+            tail.setNext(node);
+            tail = node;
+        } else {// insert at specified location
+            SingleNode tempNode = head;
+            int index = 0;
+            while (index < location - 1) {// loop till we reach specified node
+                tempNode = tempNode.getNext();
+                index++;
+            }//tempNode currently references to node after which we should insert new node
+            SingleNode nextNode = tempNode.getNext(); //this is the immediate next node after new node
+            tempNode.setNext(node);//update reference of tempNode to reference to new node
+            node.setNext(nextNode);//update newly added nodes' next.
+        }
+        setSize(getSize()+1);
+    }
+
+    void traverseLinkedList() {
+        if (existsLinkedList()) {
+            SingleNode tempNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.getValue());
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.getNext();
+            }
+        } else {
+            System.out.println("Linked List does not exists !");
+        }
+        System.out.println("\n");
+    }
+
+    public void removeElements(SingleNode head, int value) {
+        SingleNode tempnode = head;
+        int i = 0;
+        while (tempnode != null && tempnode.getNext() != null) {
+            if (tempnode.getValue() == value) {
+
+            }
+            i++;
+        }
+
+    }
+
+    public boolean existsLinkedList() {
+        return head != null;
+    }
+
+    public SingleNode getHead() {
+        return head;
+    }
+
+    public void setHead(SingleNode head) {
+        this.head = head;
+    }
+
+    public SingleNode getTail() {
+        return tail;
+    }
+
+    public void setTail(SingleNode tail) {
+        this.tail = tail;
+    }
+}
---
+++ src/SingleLinkedListMain.java
@@ -0,0 +1,16 @@
+public class SingleLinkedListMain {
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        System.out.println(list.CreateSingleLinkedList(7));
+        list.insertInLinkedList(1,0);
+        list.insertInLinkedList(2,3);
+        list.traverseLinkedList();
+    }
+}
+
+
+
+
+
+
+
